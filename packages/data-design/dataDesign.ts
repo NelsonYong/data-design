6f1d33b3b1460629241f81302927442517306a47
Yong Git <1013588891@qq.com>
2022-07-27 11:34:41 +0800

    init

--- packages/data-design/dataDesign.ts
+++ packages/data-design/dataDesign.ts
@@ -15,40 +15,31 @@ import { DataType, TransformType } from './type'
 import * as typesCheck from './dataType'
 
 export default function dataDesign() {
-	const dataType = <D = any>({
-		type,
-		target,
-	}: {
-		type: DataType
-		target: D
-	}) => {
-		if (typesCheck?.[type]) {
-			return typesCheck?.[type](target)
-		} else {
-			console.error('Type does not exist')
-		}
-	}
+  const dataType = <D = any>({ type, target }: { type: DataType; target: D }) => {
+    if (typesCheck?.[type]) {
+      return typesCheck?.[type](target)
+    } else {
+      console.error('Type does not exist')
+    }
+  }
 
-	const transform = <
-		K extends keyof TransformType = keyof TransformType,
-		T = any
-	>({
-		type,
-		data,
-		...option
-	}: {
-		type: K
-		data: T
-		option?: TransformType[K]
-	} & {
-		[P in keyof TransformType[K]]: TransformType[K][P]
-	}) => {
-		const transform = Transforms.getTransform(type)
-		return transform(data as T, option)
-	}
+  const transform = <K extends keyof TransformType = keyof TransformType, T = any>({
+    type,
+    data,
+    ...option
+  }: {
+    type: K
+    data: T
+    option?: TransformType[K]
+  } & {
+    [P in keyof TransformType[K]]: TransformType[K][P]
+  }) => {
+    const transform = Transforms.getTransform(type)
+    return transform(data as T, option)
+  }
 
-	return {
-		dataType,
-		transform,
-	}
+  return {
+    dataType,
+    transform,
+  }
 }
