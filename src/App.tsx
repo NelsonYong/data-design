153c0df25f1977e19d3c5b57ff0f330d06a48a16
Yong Git <1013588891@qq.com>
2022-10-08 15:43:37 +0800

    style: 优化结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-// import { DataType } from '@data-design/dataType'
-// import { dataType, transform } from '@data-design'
 function App() {
-  // DataType.registerDataType('isA', (d) => d === 'a')
-  // const a = dataType<'isA'>({
-  // 	type: 'isA',
-  // 	target: 'a',
-  // })
-  // console.log(a)
-
-  // const b = transform({
-  // 	type: 'filter',
-  // 	data: [1, 2, 3],
-  // 	callback(item) {
-  // 		return item > 1
-  // 	},
-  // })
-  // console.log(b)
-
-  return <div className="App">123</div>;
+	return <div>123</div>
 }
 
-export default App;
+export default App
